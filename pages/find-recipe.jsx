a9448c080e2db33f1a9b2657676c9b1a51988a7c
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 06:59:53 +0900

    feature: apply layout components to all pages and separate common head elements

---
+++ pages/find-recipe.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Recipe() {
+	return (
+		<>
+			<Head>
+				<title>Recipe Page</title>
+			</Head>
+
+			<h2>Recipe Page</h2>
+		</>
+	);
+}
