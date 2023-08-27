f4195c39c98487853222a379b7398d1063922ad3
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 05:52:44 +0900

    feature: transiton branching based on presence of child links

--- components/atoms/text/Title.jsx
+++ components/atoms/text/Title.jsx
@@ -4,8 +4,20 @@ import clsx from 'clsx';
 
 function Title({ children, url, style, className }) {
 	return (
-		<h1 className={clsx(styles.tit, className)} style={style}>
-			{url ? <Link href={url}>{children}</Link> : children}
+		<h1
+			className={clsx(styles.tit, className)}
+			//url속성유무로 자식에 링크가 있는지 파악
+			//만약 자식으로 링크가 있으면 상위요소인 h1엘리먼트에는 transition속성 제거, 자식으로 링크가 없으면 transition속성 추가
+			style={url ? style : { ...style, transitionDuration: '0.5s' }}
+		>
+			{url ? (
+				//어차피 링크가 있다면 transition이 적용되야되므로 해당 속성 추가
+				<Link href={url} style={{ transitionDuration: '0.5s' }}>
+					{children}
+				</Link>
+			) : (
+				children
+			)}
 		</h1>
 	);
 }
