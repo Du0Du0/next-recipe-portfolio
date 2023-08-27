131b61be0d68473fe9fb229c5345a1d73a055f7e
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 04:30:33 +0900

    feature: template

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: false,
+	sassOptions: {
+		includePath: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss";`,
+	},
+	images: {
+		domains: ['www.themealdb.com'],
+	},
+	compiler: { styledComponents: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
