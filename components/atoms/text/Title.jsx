295a40b9fa20d75e439ad6c53565254d303445ef
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 05:59:13 +0900

    feature: apply google font

--- components/atoms/text/Title.jsx
+++ components/atoms/text/Title.jsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 import styles from './Title.module.scss';
 import clsx from 'clsx';
+import { Nanum_Myeongjo } from 'next/font/google';
+
+const nanum = Nanum_Myeongjo({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	preload: true,
+	variable: '--font-nanum', //직접 사용할 변수명 등록, 해당 변수명을 활용하면 클래스 등록X
+});
 
 function Title({ children, url, style, className }) {
 	return (
 		<h1
-			className={clsx(styles.tit, className)}
+			//폰트객체의 클래스명을 지정하면 안쪽의 모든 폰트는 해당 폰트가 디폴트로 적용
+			//변수명을 활용해서 선별적으로 쓰고 싶을때 객체.variable
+			className={clsx(styles.tit, className, nanum.variable)}
 			//url속성유무로 자식에 링크가 있는지 파악
 			//만약 자식으로 링크가 있으면 상위요소인 h1엘리먼트에는 transition속성 제거, 자식으로 링크가 없으면 transition속성 추가
 			style={url ? style : { ...style, transitionDuration: '0.5s' }}
