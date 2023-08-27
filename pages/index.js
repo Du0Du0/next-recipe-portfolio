b15e1c49d81062dd874321740bb19ceef89f6e6e
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 04:53:31 +0900

    feature: get data such as img, name of the dish

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.scss';
 import clsx from 'clsx';
 import axios from 'axios';
+import Image from 'next/image';
 
 export default function Home({ meals }) {
-	console.log('props', meals);
+	const newMeals = meals.slice(0, 6);
+	console.log(newMeals);
+
 	return (
 		<>
 			<Head>
@@ -15,7 +18,21 @@ export default function Home({ meals }) {
 			</Head>
 
 			<main className={clsx(styles.main)}>
-				<h1>Main Page</h1>
+				<figure className='visual'>
+					<article className='bg'>
+						{newMeals.map((item) => (
+							<div key={item.idMeal} className='pic' style={{ position: 'relative', width: 400, height: 300 }}>
+								<Image src={item.strMealThumb} alt={item.strMeal} priority fill quality={50} style={{ objectFit: 'cover' }} />
+							</div>
+						))}
+					</article>
+
+					<article className='list'>
+						{newMeals.map((item) => (
+							<h2 key={item.idMeal}>{item.strMeal}</h2>
+						))}
+					</article>
+				</figure>
 			</main>
 		</>
 	);
