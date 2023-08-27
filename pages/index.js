02b4b39d1480e857ce0a0eb3e7e0a14a8c1a8116
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 04:57:50 +0900

    feature: optimize initial image size by specifying size properties

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,17 @@ export default function Home({ meals }) {
 					<article className='bg'>
 						{newMeals.map((item) => (
 							<div key={item.idMeal} className='pic' style={{ position: 'relative', width: 400, height: 300 }}>
-								<Image src={item.strMealThumb} alt={item.strMeal} priority fill quality={50} style={{ objectFit: 'cover' }} />
+								<Image
+									src={item.strMealThumb}
+									alt={item.strMeal}
+									priority
+									fill
+									quality={100}
+									style={{ objectFit: 'cover' }}
+									//이미지 속성이 fill로 지정되어 있을경우 frame크기에 상관없이 초기 로딩시 전체 브라우저 크기의 100vw크기의 용량으로 가져오기 때문에 브라우저 폭에 출력될 크기를 지정해서 이미지 성능 향상
+
+									sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+								/>
 							</div>
 						))}
 					</article>
