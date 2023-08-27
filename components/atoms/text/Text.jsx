a70d365c4f82d6fe9dfde9f9cf395e0e82bfff7a
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 06:25:03 +0900

    feature: activate component

--- components/atoms/text/Text.jsx
+++ components/atoms/text/Text.jsx
@@ -17,11 +17,18 @@ const orbitron = Orbitron({
 	variable: '--font-orbitron',
 });
 
-function Text({ children, url, style, className, type, tag = 'p' }) {
+function Text({ children, url, style, className, type, tag = 'p', isOn = false }) {
 	return React.createElement(
 		tag,
 		{
-			className: clsx(styles.txt, className, nanum.variable, orbitron.variable, styles[`tit_${type}`]),
+			className: clsx(
+				styles.txt,
+				className,
+				nanum.variable,
+				orbitron.variable,
+				styles[`txt_${type}`], //추가 클래스명
+				isOn && styles.on //전달되는 boolean값에 따라 고유클래스 on추가, module.sass가 자체적으로 고유클래스명으로 변환하기 때문에 부모의 클래스명을 내부 전용 css에 연결하는게 불가
+			),
 			style: url ? style : { ...style, transitionDuration: '0.5s' },
 			onMouseEnter: (e) => (e.target.style.color = style?.hoverColor),
 			onMouseLeave: (e) => (e.target.style.color = style?.color),
