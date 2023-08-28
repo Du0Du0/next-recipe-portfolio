8b5ffc3fc134ae9b32f0dfd05c6d8dc9418dcbcc
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 23:44:59 +0900

    fetaure: set default components for page change motion

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,23 @@ import '@/styles/globals.scss';
 import axios from 'axios';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useRouter } from 'next/router';
+import { motion, AnimatePresence } from 'framer-motion';
 
 axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
+	const router = useRouter();
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<AnimatePresence mode='wait'>
+				<motion.div key={router.pathname}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</motion.div>
+			</AnimatePresence>
 			<ReactQueryDevtools />
 		</QueryClientProvider>
 	);
