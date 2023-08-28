909ba84ef2f7c7f7fba11f742c831ff1a1009e48
do-hyun,kim <dohyun911@hanmail.net>
2023-08-28 18:04:06 +0900

    fetaure: react query setting

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import Layout from '@/components/template/Layout/Layout';
 import '@/styles/globals.scss';
 import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<QueryClientProvider client={queryClient}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
